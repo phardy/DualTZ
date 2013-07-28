f0dac0cdbb5f379107f78af423ee0b6dda497e8a
Peter Hardy <peter@hardy.dropbear.id.au>
2013-07-28 15:56:44 +1000

    Cleaning up and resizing, preparing for httpebble.

--- src/Dual-TZ.c
+++ src/Dual-TZ.c
@@ -2,7 +2,6 @@
 #include "pebble_app.h"
 #include "pebble_fonts.h"
 
-
 #define MY_UUID { 0x33, 0x1D, 0x7F, 0x32, 0x4F, 0xEE, 0x4D, 0x6C, 0xBD, 0x95, 0xE2, 0x7C, 0x6C, 0xDB, 0x44, 0x73 }
 PBL_APP_INFO(MY_UUID,
              "Dual-TZ", "Kids, Inc.",
@@ -18,19 +17,19 @@ Layer AnalogueHourLayer, AnalogueMinuteLayer;
 GPath AnalogueHourPath, AnalogueMinutePath;
 TextLayer TZName;
 TextLayer TZOffset;
-TextLayer TZTime;
-TextLayer TZTimeS;
+TextLayer DigitalTime;
+TextLayer DigitalTimeS;
 static GFont TZFont;
-static GFont TimeFont;
-static GFont TimeSFont;
+static GFont DigitalTimeFont;
+static GFont DigitalTimeSFont;
 
 // Time rememberating stuff.
 // Currently hardcoded. Sorry.
-char TZNameText[] = "Time zone label"; // max 15 characters (arbitrary)
-char TZOffsetText[] = "+10";
-static char TZTimeText[] = "00:00";
-static char TZTimeSText[] = "00";
-static char *TZFormat;
+char TZNameText[] = "Kolkata"; // max 15 characters (arbitrary)
+char TZOffsetText[] = "+5:30"; // 5 characters (for now)
+static char DigitalTimeText[] = "00:00";
+static char DigitalTimeSText[] = "00";
+static char *DigitalTimeFormat;
 
 const GPathInfo HOUR_HAND_PATH_POINTS = {
   5,
@@ -104,16 +103,18 @@ void minute_display_layer_update_callback (Layer *me, GContext* ctx) {
 }
 
 void handle_second_tick(AppContextRef ctx, PebbleTickEvent *t) {
-  string_format_time(TZTimeSText, sizeof(TZTimeSText), "%S", t->tick_time);
-  text_layer_set_text(&TZTimeS, TZTimeSText);
+  string_format_time(DigitalTimeSText, sizeof(DigitalTimeSText),
+		     "%S", t->tick_time);
+  text_layer_set_text(&DigitalTimeS, DigitalTimeSText);
 
   if (t->tick_time->tm_sec == 0) {
     layer_mark_dirty(&AnalogueMinuteLayer);
     if (t->tick_time->tm_min % 2 == 0) {
       layer_mark_dirty(&AnalogueHourLayer);
     }
-    string_format_time(TZTimeText, sizeof(TZTimeText), TZFormat, t->tick_time);
-    text_layer_set_text(&TZTime, TZTimeText);
+    string_format_time(DigitalTimeText, sizeof(DigitalTimeText),
+		       DigitalTimeFormat, t->tick_time);
+    text_layer_set_text(&DigitalTime, DigitalTimeText);
   }
 }
 
@@ -121,26 +122,26 @@ void display_init(AppContextRef *ctx) {
   // load resources
   resource_init_current_app(&APP_RESOURCES);
   TZFont = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_DIGITAL_14));
-  TimeFont = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_DIGITAL_40));
-  TimeSFont = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_DIGITAL_20));
+  DigitalTimeFont = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_DIGITAL_40));
+  DigitalTimeSFont = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_DIGITAL_20));
 
   // init root window
   window_init(&window, "Root window");
   window_stack_push(&window, true /* Animated */);
 
   // main time display
-  text_layer_init(&TZTime, GRect(17, 127, 90, 40)); // sizing hacky as hell
-  text_layer_set_text_alignment(&TZTime, GTextAlignmentRight);
-  text_layer_set_text_color(&TZTime, GColorBlack);
-  text_layer_set_font(&TZTime, TimeFont);
-  layer_add_child(&window.layer, &TZTime.layer);
+  text_layer_init(&DigitalTime, GRect(17, 127, 90, 40)); // sizing hacky as hell
+  text_layer_set_text_alignment(&DigitalTime, GTextAlignmentRight);
+  text_layer_set_text_color(&DigitalTime, GColorBlack);
+  text_layer_set_font(&DigitalTime, DigitalTimeFont);
+  layer_add_child(&window.layer, &DigitalTime.layer);
 
   // seconds display
-  text_layer_init(&TZTimeS, GRect(109, 147, 20, 20)); // sizing made me cry
-  text_layer_set_text_alignment(&TZTimeS, GTextAlignmentLeft);
-  text_layer_set_text_color(&TZTimeS, GColorBlack);
-  text_layer_set_font(&TZTimeS, TimeSFont);
-  layer_add_child(&window.layer, &TZTimeS.layer);
+  text_layer_init(&DigitalTimeS, GRect(109, 147, 20, 20)); // sizing made me cry
+  text_layer_set_text_alignment(&DigitalTimeS, GTextAlignmentLeft);
+  text_layer_set_text_color(&DigitalTimeS, GColorBlack);
+  text_layer_set_font(&DigitalTimeS, DigitalTimeSFont);
+  layer_add_child(&window.layer, &DigitalTimeS.layer);
 
   // timezone name display
   text_layer_init(&TZName, GRect(14, 125, 95, 15));
@@ -150,7 +151,7 @@ void display_init(AppContextRef *ctx) {
   layer_add_child(&window.layer, &TZName.layer);
 
   // timezone offset display
-  text_layer_init(&TZOffset, GRect(110, 125, 20, 15));
+  text_layer_init(&TZOffset, GRect(110, 125, 40, 15));
   text_layer_set_text_alignment(&TZOffset, GTextAlignmentLeft);
   text_layer_set_text_color(&TZOffset, GColorBlack);
   text_layer_set_font(&TZOffset, TZFont);
@@ -179,20 +180,24 @@ void handle_init(AppContextRef ctx) {
   display_init(&ctx);
 
   if (clock_is_24h_style()) {
-    TZFormat = "%H:%M";
+    DigitalTimeFormat = "%H:%M";
   } else {
-    TZFormat = "%I:%M";
+    DigitalTimeFormat = "%I:%M";
   }
 
   // write current time to display
   PblTm curTime;
   get_time(&curTime);
-  string_format_time(TZTimeSText, sizeof(TZTimeSText), "%S", &curTime);
-  string_format_time(TZTimeText, sizeof(TZTimeText), TZFormat, &curTime);
-  text_layer_set_text(&TZTime, TZTimeText);
-  text_layer_set_text(&TZTimeS, TZTimeSText);
-  text_layer_set_text(&TZName, TZNameText);
-  text_layer_set_text(&TZOffset, TZOffsetText);
+  string_format_time(DigitalTimeSText, sizeof(DigitalTimeSText),
+		     "%S", &curTime);
+  string_format_time(DigitalTimeText, sizeof(DigitalTimeText),
+		     DigitalTimeFormat, &curTime);
+  text_layer_set_text(&DigitalTime, DigitalTimeText);
+  text_layer_set_text(&DigitalTimeS, DigitalTimeSText);
+  // text_layer_set_text(&TZName, TZNameText);
+  // text_layer_set_text(&TZOffset, TZOffsetText);
+  text_layer_set_text(&TZName, "local time");
+  text_layer_set_text(&TZOffset, " ");
 
   // draw analogue hands
   layer_mark_dirty(&AnalogueMinuteLayer);
