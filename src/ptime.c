3ac9b6c0f7ad8de43e2bece1fe5a78735db98cfe
Peter Hardy <peter@hardy.dropbear.id.au>
2013-07-31 01:03:57 +1000

    Larger data types in plocaltime

--- src/ptime.c
+++ src/ptime.c
@@ -77,8 +77,8 @@ PblTm plocaltime (const time_t *timep) {
   PblTm res;
   long days, rem;
 
-  days = ((long)timep) / SECSPERDAY;
-  rem = ((long)timep) % SECSPERDAY;
+  days = ((int32_t)timep) / SECSPERDAY;
+  rem = ((int32_t)timep) % SECSPERDAY;
   while (rem < 0) {
     rem += SECSPERDAY;
     --days;
