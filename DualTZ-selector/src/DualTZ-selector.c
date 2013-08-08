13b419ac22ff2a6a9b4093b4e9c667e2892703f3
Peter Hardy <peter@hardy.dropbear.id.au>
2013-08-08 16:14:56 +1000

    uh

--- DualTZ-selector/src/DualTZ-selector.c
+++ DualTZ-selector/src/DualTZ-selector.c
@@ -136,7 +136,9 @@ void region_menu_select_callback(MenuLayer *me, MenuIndex *cell_index,
 
 uint16_t zone_menu_get_num_rows_callback(MenuLayer *me,
 					 uint16_t section_index, void *data) {
-  return tz_count+1;
+  // return tz_count;
+  // Hardcoded for Australia
+  return 12;
 }
 
 void zone_menu_draw_row_callback(GContext* ctx, const Layer *cell_layer,
