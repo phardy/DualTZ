b47ab374ee48a4a7f03d11090c28322abbf0d495
Peter Hardy <peter@hardy.dropbear.id.au>
2013-08-08 14:50:46 +1000

    First pass at zone menu. It's all fucked.

--- DualTZ-selector/src/DualTZ-selector.c
+++ DualTZ-selector/src/DualTZ-selector.c
@@ -22,28 +22,61 @@ PBL_APP_INFO(HTTP_UUID,
 
 #define BUF_SIZE 1024
 
-// Number of TZ regions
-#define NUM_REGIONS 10
-
-// Longest file is 143, most are much shorter.
-#define MAX_TZ 150
+// Longest file is 125, most are much shorter.
+#define MAX_TZ 130
 #define TZ_NAME_LEN 15
 #define TZ_OFFSET_LEN 5
 
+Window root_window;
+Window region_window;
+Window zone_window;
+MenuLayer root_menu;
+MenuLayer region_menu;
+MenuLayer zone_menu;
+
+TZInfo DisplayTZ;
+
+// Number of TZ regions
+#define NUM_REGIONS 10
+typedef char * string;
+static string regions[NUM_REGIONS];
+uint32_t region_resources[NUM_REGIONS] = {
+  RESOURCE_ID_AFRICA_TZ,
+  RESOURCE_ID_AMERICA_TZ,
+  RESOURCE_ID_ANTARCTICA_TZ,
+  RESOURCE_ID_ARCTIC_TZ,
+  RESOURCE_ID_ASIA_TZ,
+  RESOURCE_ID_AUSTRALIA_TZ,
+  RESOURCE_ID_ATLANTIC_TZ,
+  RESOURCE_ID_EUROPE_TZ,
+  RESOURCE_ID_INDIAN_TZ,
+  RESOURCE_ID_PACIFIC_TZ
+};
 // tz_index contains offsets for the start of
 // each line in the current resource (tz file)
 uint16_t tz_index[MAX_TZ];
 uint16_t tz_count;
 
-Window root_window;
-Window region_window;
-MenuLayer root_menu;
-MenuLayer region_menu;
+uint8_t filebuf[BUF_SIZE];
+ResHandle current_resource_handle;
+void read_file(uint32_t resource_id) {
+  current_resource_handle = resource_get_handle(resource_id);
+  uint16_t bytesread = BUF_SIZE+1;
+  uint16_t index = 0;
+  tz_count=0;
+  tz_index[tz_count] = index;
+  tz_count++;
+  while (bytesread >= BUF_SIZE) {
+    bytesread = resource_load_byte_range(current_resource_handle, index, filebuf, BUF_SIZE);
+    for (int i=0; i < bytesread; i++) {
+      if (filebuf[i] == '\n') {
+	tz_index[tz_count] = index;
+	tz_count++;
+      }
+    }
+  }
+}
 
-TZInfo DisplayTZ;
-
-typedef char * string;
-static string regions[NUM_REGIONS];
 uint16_t root_menu_get_num_rows_callback(MenuLayer *me,
 					 uint16_t section_index, void *data) {
   return 2;
@@ -91,23 +124,33 @@ void region_menu_draw_row_callback(GContext* ctx, const Layer *cell_layer,
   menu_cell_basic_draw(ctx, cell_layer, regions[cell_index->row], NULL, NULL);
 }
 
-uint8_t filebuf[BUF_SIZE];
-void read_file(void) {
-  ResHandle fh = resource_get_handle(RESOURCE_ID_ARCTIC_TZ);
-  uint16_t bytesread = BUF_SIZE+1;
-  uint16_t index = 0;
-  tz_count=0;
-  tz_index[tz_count] = index;
-  tz_count++;
-  while (bytesread >= BUF_SIZE) {
-    bytesread = resource_load_byte_range(fh, index, filebuf, BUF_SIZE);
-    for (int i=0; i < bytesread; i++) {
-      if (filebuf[i] == '\n') {
-	tz_index[tz_count] = index;
-	tz_count++;
-      }
+void region_menu_select_callback(MenuLayer *me, MenuIndex *cell_index,
+				 void *data) {
+  // reindex for the selected zone
+  read_file(cell_index->row);
+  // display the zone window
+  window_stack_push(&zone_window, true);
+}
+
+uint16_t zone_menu_get_num_rows_callback(MenuLayer *me,
+					 uint16_t section_index, void *data) {
+  return tz_count+1;
+}
+
+void zone_menu_draw_row_callback(GContext* ctx, const Layer *cell_layer,
+				   MenuIndex *cell_index, void *data) {
+  uint8_t tz_name[TZ_NAME_LEN+1];
+  resource_load_byte_range(current_resource_handle, 0, tz_name, TZ_NAME_LEN+1);
+  for (int x=0; x == TZ_NAME_LEN; x++) {
+    if (tz_name[x] == '_')
+      tz_name[x] = ' ';
+    else if (tz_name[x] == ' ') {
+      tz_name[x] = '\0';
+      break;
     }
   }
+  tz_name[TZ_NAME_LEN] = '\0';
+  menu_cell_basic_draw(ctx, cell_layer, (char*)tz_name, NULL, NULL);
 }
 
 void handle_init(AppContextRef ctx) {
@@ -130,19 +173,23 @@ void handle_init(AppContextRef ctx) {
   menu_layer_set_callbacks(&region_menu, NULL, (MenuLayerCallbacks){
       .get_num_rows = region_menu_get_num_rows_callback,
 	.draw_row = region_menu_draw_row_callback,
-	});
+	.select_click = region_menu_select_callback
+	 });
   menu_layer_set_click_config_onto_window(&region_menu, &region_window);
   layer_add_child(&region_window.layer, menu_layer_get_layer(&region_menu));
 
+  // Setup for zone window
+  window_init(&zone_window, "Zone select");
+  menu_layer_init(&zone_menu, zone_window.layer.bounds);
+  menu_layer_set_callbacks(&zone_menu, NULL, (MenuLayerCallbacks){
+      .get_num_rows = zone_menu_get_num_rows_callback,
+	.draw_row = zone_menu_draw_row_callback,
+	});
+  menu_layer_set_click_config_onto_window(&zone_menu, &zone_window);
+  layer_add_child(&zone_window.layer, menu_layer_get_layer(&zone_menu));
+
   // Push root window to bottom of stack
   window_stack_push(&root_window, true /* Animated */);
-
-  // window_set_window_handlers(&root_window, (WindowHandlers){
-  //     .load = root_window_load,
-  // 	});
-  // window_set_window_handlers(&region_window, (WindowHandlers){
-  //     .load = region_window_load,
-  // });
 
   // Right now, hard-code to displaying UTC always
   strcpy(DisplayTZ.tz_name, UTC.tz_name);
