34928d872eb4ebc99aab7807d37129fd898afe74
Peter Hardy <peter@hardy.dropbear.id.au>
2013-11-12 18:35:44 +1100

    More file cleanup.

--- src/PDutils.h
+++ src/PDutils.h
@@ -1,1 +1,10 @@
-../../common/PDutils.h
+/*
+  This code is derived from PDPCLIB, the public domain C runtime
+  library by Paul Edwards. http://pdos.sourceforge.net/
+
+  This code is released to the public domain.
+*/
+
+time_t pmktime (PblTm *tmptr);
+PblTm *pgmtime (const time_t *timer);
+char *pstrtok(char *s1, const char *s2);
--- src/common.h
+++ src/common.h
@@ -1,1 +1,22 @@
-../../common/common.h
+// Functions and structures used by both apps
+
+#define TZ_NAME_LEN 15
+#define TZ_OFFSET_LEN 6
+typedef struct {
+  char tz_name[TZ_NAME_LEN+1];
+  int8_t tz_hours;
+  int8_t tz_minutes;
+  bool tz_dst;
+} TZInfo;
+
+typedef enum {
+  local,
+  utc,
+  remote
+} TZState;
+
+// static TZInfo UTC = {"UTC", 0, 0, NULL};
+
+void format_timezone(TZInfo *tz, char *str);
+
+void parse_timezone(char *str, TZInfo *tz);
