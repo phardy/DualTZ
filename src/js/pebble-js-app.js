36722da1d6a788946c19543e8c4b4a8ffa4ec6a0
Peter Hardy <peter@hardy.dropbear.id.au>
2013-11-18 23:49:24 +1100

    Send params to phoneapp, parse and store.

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,4 +1,23 @@
+function appMessageAck(e) {
+    console.log('options sent to Pebble successfully');
+}
+
+function appMessageNack(e) {
+    console.log('options not sent to Pebble: ' + e.error.message);
+}
+
 Pebble.addEventListener("showConfiguration", function() {
-    console.log("showing configuration");
-    Pebble.openURL("http://hardy.dropbear.id.au/DualTZ/");
+    console.log('showing configuration');
+    Pebble.openURL('http://hardy.dropbear.id.au/DualTZ/');
+});
+
+Pebble.addEventListener("webviewclosed", function(e) {
+    console.log('configuration closed');
+    if (e.response != '') {
+	var params = JSON.parse(decodeURIComponent(e.response));
+	console.log(params);
+	window.localStorage.setItem('remote-tz-name' + params['remote-tz-name']);
+	window.localStorage.setItem('remote-tz-offset' + params['remote-tz-offset']);
+	window.localStorage.setItem('local-tz-offset' + params['local-tz-offset']);
+    }
 });
