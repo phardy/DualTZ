0bd1b4ce9261af913d11dc935248358cf7f4e317
Peter Hardy <peter@hardy.dropbear.id.au>
2013-11-19 01:28:54 +1100

    Simplified TZInfo struct. Updated formatter to handle. Removed unused parser.

--- src/tz.c
+++ src/tz.c
@@ -6,53 +6,20 @@
 #include "tz.h"
 
 void format_timezone(TZInfo *tz, char *str) {
+  int tz_hours = tz->tz_offset / 60;
+  int tz_minutes = tz->tz_offset % 60;
   // xprintf doesn't seem to support the + formatting flag
-  if (tz->tz_hours < 0) {
-    if (tz->tz_minutes == 0) {
-      sprintf(str, "%02d", tz->tz_hours);
+  if (tz_hours < 0) {
+    if (tz_minutes == 0) {
+      sprintf(str, "%02d", tz_hours);
     } else {
-      sprintf(str, "%02d:%02d", tz->tz_hours, tz->tz_minutes);
+      sprintf(str, "%02d:%02d", tz_hours, tz_minutes);
     }
   } else {
-    if (tz->tz_minutes == 0) {
-      sprintf(str, "+%02d", tz->tz_hours);
+    if (tz_minutes == 0) {
+      sprintf(str, "+%02d", tz_hours);
     } else {
-      sprintf(str, "+%02d:%02d", tz->tz_hours, tz->tz_minutes);
+      sprintf(str, "+%02d:%02d", tz_hours, tz_minutes);
     }
-  }
-}
-
-// Parses a TZ string "TZ name\tTZ offset"
-// in to the given TZInfo struct.
-void parse_timezone(char *str, TZInfo *tz) {
-  const char sep[] = "\t:";
-  const char *token;
-  // Extract TZ name
-  token = p_strtok(str, sep);
-  if (token != NULL) {
-    strncpy(tz->tz_name, token, TZ_NAME_LEN);
-    tz->tz_name[TZ_NAME_LEN] = '\0'; // In case we have a long name
-  }
-  // Extract TZ offset
-  token = p_strtok(NULL, sep);
-  if (token != NULL) {
-    long tz_hours = p_strtol(token, (char **)NULL, 10);
-    tz->tz_hours = (int8_t)tz_hours;
-  } else {
-    tz->tz_hours = 0;
-  }
-  token = p_strtok(NULL, sep);
-  if (token != NULL) {
-    long tz_minutes = p_strtol(token, (char **)NULL, 10);
-    tz->tz_minutes = (int8_t)tz_minutes;
-  } else {
-    tz->tz_minutes = 0;
-  }
-
-  token = p_strtok(NULL, sep);
-  if (token != NULL && token[0] == '1') {
-    tz->tz_dst = true;
-  } else {
-    tz->tz_dst = false;
   }
 }
--- src/tz.h
+++ src/tz.h
@@ -2,13 +2,9 @@
 #define TZ_OFFSET_LEN 6
 typedef struct {
   char tz_name[TZ_NAME_LEN+1];
-  int8_t tz_hours;
-  int8_t tz_minutes;
-  bool tz_dst;
+  int32_t tz_offset;
 } TZInfo;
 
 // static TZInfo UTC = {"UTC", 0, 0, NULL};
 
 void format_timezone(TZInfo *tz, char *str);
-
-void parse_timezone(char *str, TZInfo *tz);
