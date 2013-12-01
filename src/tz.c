de3431934c3eb65c34b1519f227670d2bd79c5f9
Peter Hardy <peter@hardy.dropbear.id.au>
2013-12-01 14:24:21 +1100

    Removed redundant snprintf calls.

--- src/tz.c
+++ src/tz.c
@@ -7,21 +7,10 @@
 
 void format_timezone(int32_t offset, char *str) {
   int tz_hours = offset / 3600;
-  // TODO: test this - not sure if 3600 or 60.
   int tz_minutes = offset % 3600;
-  // TODO: Update these. Probably not requried any more.
-  // xprintf doesn't seem to support the + formatting flag
-  if (tz_hours < 0) {
-    if (tz_minutes == 0) {
-      snprintf(str, TZ_OFFSET_LEN, "%02d", tz_hours);
-    } else {
-      snprintf(str, TZ_OFFSET_LEN, "%02d:%02d", tz_hours, tz_minutes);
-    }
+  if (tz_minutes == 0) {
+    snprintf(str, TZ_OFFSET_LEN, "%+02d", tz_hours);
   } else {
-    if (tz_minutes == 0) {
-      snprintf(str, TZ_OFFSET_LEN, "+%02d", tz_hours);
-    } else {
-      snprintf(str, TZ_OFFSET_LEN, "+%02d:%02d", tz_hours, tz_minutes);
-    }
+    snprintf(str, TZ_OFFSET_LEN, "%+02d:%02d", tz_hours, tz_minutes);
   }
 }
