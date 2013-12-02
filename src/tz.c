b99d5d4be6278590ffcbe873bcaddced64929af8
Peter Hardy <peter@hardy.dropbear.id.au>
2013-12-03 01:49:27 +1100

    Fixed tz formatting bug, tweaked tz display format.

--- src/tz.c
+++ src/tz.c
@@ -6,11 +6,13 @@
 #include "tz.h"
 
 void format_timezone(int32_t offset, char *str) {
-  int tz_hours = offset / 3600;
-  int tz_minutes = offset % 3600;
-  if (tz_minutes == 0) {
-    snprintf(str, TZ_OFFSET_LEN, "%+02d", tz_hours);
-  } else {
-    snprintf(str, TZ_OFFSET_LEN, "%+02d:%02d", tz_hours, tz_minutes);
-  }
+  offset = offset/60;
+  int tz_hours = offset / 60;
+  int tz_minutes = offset % 60;
+  snprintf(str, TZ_OFFSET_LEN, "%+02d%02d", tz_hours, tz_minutes);
+  // if (tz_minutes == 0) {
+  //   snprintf(str, TZ_OFFSET_LEN, "%+02d", tz_hours);
+  // } else {
+  //   snprintf(str, TZ_OFFSET_LEN, "%+02d:%02d", tz_hours, tz_minutes);
+  // }
 }
